dcd43a252435c93287ae6d613111fec62d6af786
2015lym <18622797149@163.com>
2017-12-26 13:28:15 +0800

    更新

--- 第7章/Chapter7/src/pages/infinite/infinite.ts
+++ 第7章/Chapter7/src/pages/infinite/infinite.ts
@@ -7,8 +7,30 @@ import { NavController } from 'ionic-angular';
 })
 export class InfinitePage {
 
-  constructor(public navCtrl: NavController) {
+  // 列表数组
+  private items: string[] = [];
+  // 数据
+  private tagNumber: number = 1;
 
+  constructor(public navCtrl: NavController) { }
+
+  ionViewDidLoad() {
+    for (let i = 1; i < 11; i++) {
+      this.items.push('第' + this.tagNumber + '组  第' + i + '条');
+    }
   }
 
+  doInfinite(infiniteScroll) {
+    console.log('开始滚动');
+
+    setTimeout(() => {
+      this.tagNumber++;
+      for (let i = 1; i < 11; i++) {
+        this.items.push('第' + this.tagNumber + '组  第' + i + '条');
+      }
+
+      console.log('滚动结束');
+      infiniteScroll.complete();
+    }, 500);
+  }
 }
--- 第7章/Chapter7/src/pages/refresher/refresher.ts
+++ 第7章/Chapter7/src/pages/refresher/refresher.ts
@@ -7,8 +7,20 @@ import { NavController } from 'ionic-angular';
 })
 export class RefresherPage {
 
-  constructor(public navCtrl: NavController) {
+  // 列表数组
+  private items: string[] = [];
 
+  constructor(public navCtrl: NavController) { }
+
+  doRefresh(refresher) {
+    console.log('开始刷新', refresher);
+
+    setTimeout(() => {
+      for (let i = 1; i < 11; i++) {
+        this.items.push('第' + i + '条数据');
+      }
+      console.log('刷新结束');
+      refresher.complete();
+    }, 2000);
   }
-
 }
