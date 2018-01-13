b78283f52a29746c6d340d8493466d260913e890
2015lym <18622797149@163.com>
2018-01-13 21:43:30 +0800

    更新

--- 第10章/Chapter10/src/pages/contact/contact.ts
+++ 第10章/Chapter10/src/pages/contact/contact.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +7,12 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    public platform: Platform) {
   }
 
+  isAndroid(): boolean {
+    return this.platform.is('android');
+  }
 }
